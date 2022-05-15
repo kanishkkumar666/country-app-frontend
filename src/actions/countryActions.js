8eb5142ec87b88bb673923ac6c32504106797649
kanishksoftobiz <kanishk@softobiz.com>
2022-05-15 21:20:16 +0530

    initial commit

--- src/actions/countryActions.js
+++ src/actions/countryActions.js
@@ -43,9 +43,7 @@ export const listCountrys =
     try {
       dispatch({ type: COUNTRY_LIST_REQUEST });
       const { data } = await axios.get(
-        `https://country-app-backends.herokuapp.com/api/country?keyword=${keyword}&pageNumber=${pageNumber}`,{ 
-          mode: 'no-cors' // 'cors' by default
-      }
+        `https://country-app-backends.herokuapp.com/api/country?keyword=${keyword}&pageNumber=${pageNumber}`,
       );
       dispatch({
         type: COUNTRY_LIST_SUCCESS,
@@ -65,9 +63,7 @@ export const listCountrys =
 export const listCountryDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: COUNTRY_DETAILS_REQUEST });
-    const { data } = await axios.get(`https://country-app-backends.herokuapp.com/api/country/${id}`,{ 
-      mode: 'no-cors' // 'cors' by default
-  });
+    const { data } = await axios.get(`https://country-app-backends.herokuapp.com/api/country/${id}`);
     dispatch({
       type: COUNTRY_DETAILS_SUCCESS,
       payload: data,
@@ -99,9 +95,7 @@ export const deleteCountry = (id) => async (dispatch, getState) => {
       },
     };
 
-    await axios.delete(`https://country-app-backends.herokuapp.com/api/country/${id}`,{ 
-      mode: 'no-cors' // 'cors' by default
-  }, config);
+    await axios.delete(`https://country-app-backends.herokuapp.com/api/country/${id}`, config);
 
     dispatch({
       type: COUNTRY_DELETE_SUCCESS,
@@ -133,9 +127,7 @@ export const createCountry = () => async (dispatch, getState) => {
       },
     };
 
-    const { data } = await axios.post(`https://country-app-backends.herokuapp.com/api/country`,{ 
-      mode: 'no-cors' // 'cors' by default
-  }, {}, config);
+    const { data } = await axios.post(`https://country-app-backends.herokuapp.com/api/country`, {}, config);
 
     dispatch({
       type: COUNTRY_CREATE_SUCCESS,
@@ -170,9 +162,7 @@ export const updateCountry = (country) => async (dispatch, getState) => {
     };
 
     const { data } = await axios.put(
-      `https://country-app-backends.herokuapp.com/api/country/${country._id}`,{ 
-        mode: 'no-cors' // 'cors' by default
-    },
+      `https://country-app-backends.herokuapp.com/api/country/${country._id}`,
       country,
       config
     );
